0aa5cc3bf5249de38bb75de7eaccd408450b2a4a
Ineffable <83691312+Eriel-Jesus@users.noreply.github.com>
2026-03-20 19:47:53 -0300

    Add files via upload

---
+++ Threads/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Thread threadPing = new Thread(() => ExecutarMensagens("PING", 10, 180));
+            Thread threadPong = new Thread(() => ExecutarMensagens("PONG", 10, 200));
+
+            Console.WriteLine("Iniciando...");
+            threadPing.Start();
+
+            threadPong.Start();    
+        }
+
+        static void ExecutarMensagens(string texto, int vezes, int delay)
+        {
+            for (int i = 0; i < vezes; i++)
+            {
+                Console.WriteLine(texto);
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
